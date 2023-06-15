73cd8babbb7b239cef018c0c5ec811d61e4f9575
Sanaullah <webdevfarsi@gmail.com>
2023-06-15 23:28:03 +0430

    index file changed

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { userMessagerouter } from './routes/getUserMessageRoute.js';
 import cors from 'cors';
-import path from 'path';
 import morgan from 'morgan';
 
-const __dirname = path.resolve();
-console.log("dirname is:",path.join(__dirname,"./naw_profile/build"))
 const app = express();
 app.use(express.json())
 dotenv.config();
 app.use(cors())
 app.use(morgan('dev'))
 
-app.use(express.static(path.join(__dirname,"./new_profile/build")));
-
-app.get("*",function(_,res){
-    res.sendFile(path.join(__dirname,"./new_profile/build/index.html"),function(error){
-        res.status(500).send(error)
-    })
-})
 
 // routes 
 app.use('/gmail',userMessagerouter)
